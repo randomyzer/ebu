5c2217789a40b11bc033e7a1ce9b72382b3eacc3
Randomyzer <randomyzer@gmail.com>
2013-11-11 23:37:49 -0500

    Update

--- app/views/voting.js
+++ app/views/voting.js
@@ -14,7 +14,7 @@ define(function (require) {
         el: '.content',
         initialize: function () {
             this.listenTo(this.collection, 'sync', this.render);
-            this.collection.fetch();
+            this.collection.fetch({data: {table_id: this.options.tableId, type: 2}});
         },
         render: function () {
             this.$el.html(template({
